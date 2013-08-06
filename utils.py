86278f694085b8dfc84d602dff180dff81ef343b
ravall <v.semenov@alt1.ru>
2013-08-06 16:17:15 +0400

    правки

--- utils.py
+++ utils.py
@@ -11,8 +11,12 @@ def api_request(url):
     """
     обращаемся по апи к sancta_py
     вынесен отдельно, т.к разные проекты могут использовать его
-    требуется установка CACHE_API_TIMEOUT в настройках
+    требуется установка в настройках
+        CACHE_API_TIMEOUT (время сохранения кэша)
+        API_URL базовый урл до api
+        и настроек кэширования
     """
+    url = '{0}/api/{1}'.format(settings.API_URL, url)
     cache_key = md5(url).hexdigest()
     try:
         request = requests.get(url)
@@ -24,5 +28,5 @@ def api_request(url):
             cache.set(cache_key, result, settings.CACHE_API_TIMEOUT)
     except requests.exceptions.RequestException:
         # если произошла ошибка - берем из Кэша
-        result = cache.get(cache_key, ''):
+        result = cache.get(cache_key, '')
     return result
