0505238ff46143c3d44dd005672f0e779578af92
Ravall <valery.ravall@gmail.com>
2013-09-04 14:58:32 +0400

    fix

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from fabric.api import *
+from time import sleep
 
 env.activate = 'source /home/envs/admin_sancta/bin/activate'
 env.hosts = ['root@78.47.157.221']
+
 
 def deploy(tag=None):
     if not tag:
@@ -15,7 +17,7 @@ def deploy(tag=None):
             run("git checkout {0}".format(tag))
             run("git submodule update --init")
             run("cat release")
-            run("pip install -r files/pip.freeze")
+            run("pip install -r control/pip.freeze")
             run("find . -name '*.pyc' -delete")
             run("python sancta/manage.py syncdb  --settings=config.admin")
             run("python sancta/manage.py syncdb  --settings=config.admin --database=sancta_db")
@@ -25,10 +27,32 @@ def deploy(tag=None):
             restart()
 
 
+def deploy_engdel(tag=None):
+    if not tag:
+        print "для выкладки нужен тег"
+    print "выкладываем тег {0}".format(tag)
+    with cd('/home/web/engdel.ru'):
+        with prefix(env.activate):
+            run("git fetch")
+            run("git remote prune origin")
+            run("git checkout {0}".format(tag))
+            run("git submodule update --init")
+            run("pip install -r control/pip.freeze")
+            run("find . -name '*.pyc' -delete")
+            run("python sancta/manage.py syncdb")
+            run("python sancta/manage.py migrate --merge")
+            run("python sancta/manage.py compass")
+            run("python sancta/manage.py collectstatic --noinput")
+            restart()
+
+
 def restart():
     run('service nginx restart')
     # gunicorn
-    run('service supervisor restart')
+    run('service supervisor stop')
+    sleep(3)
+    run('service supervisor start')
     #celery
     run('service celeryd stop')
+    sleep(1)
     run('service celeryd start')
